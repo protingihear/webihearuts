d8d9edd1f9c0e23addb43407a6b33558aa4dcd26
purianagatha <purianagatha@gmail.com>
2024-10-27 17:24:59 +0700

    telah membuat page scan to text

---
+++ ihear_webpro/puri_page/src/js/scanToText.js
@@ -0,0 +1,5 @@
+// Tambahkan JavaScript di sini jika diperlukan
+// Misalnya, untuk menambah fungsi ketika tombol kamera ditekan
+document.querySelector('.camera-button').addEventListener('click', function() {
+    alert("Button kamera ditekan!");
+});

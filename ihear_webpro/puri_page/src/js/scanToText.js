cb4ea2327098fb304b7210640e2266eb9d762b00
purianagatha <purianagatha@gmail.com>
2024-11-13 17:08:38 +0700

    menambahkan page information

--- ihear_webpro/puri_page/src/js/scanToText.js
+++ ihear_webpro/puri_page/src/js/scanToText.js
@@ -1,5 +1,3 @@
-// Tambahkan JavaScript di sini jika diperlukan
-// Misalnya, untuk menambah fungsi ketika tombol kamera ditekan
 document.querySelector('.camera-button').addEventListener('click', function () {
     const infoSection = document.getElementById("infoSection");
     const message = document.createElement("div");
@@ -7,3 +5,28 @@ document.querySelector('.camera-button').addEventListener('click', function () {
     infoSection.innerHTML = "";
     infoSection.appendChild(message);
 });
+
+// // Tambahkan elemen popup
+// const popupOverlayCamera = document.createElement('div');
+// popupOverlayCamera.classList.add('popup-overlay');
+// popupOverlayCamera.innerHTML = `
+//     <div class="popup-content">
+//         <h2>Fitur sedang dikerjakan</h2>
+//         <p>Mohon tunggu, fitur ini akan segera hadir.</p>
+//         <button id="closePopupCamera">Tutup</button>
+//     </div>
+// `;
+// document.body.appendChild(popupOverlayCamera);
+// function showPopupCamera() {
+//     popupOverlayCamera.style.display = 'flex';
+// }
+
+// function hidePopupCamera() {
+//     popupOverlayCamera.style.display = 'none';
+// }
+
+// document.getElementById('closePopupCamera').addEventListener('click', hidePopupCamera);
+
+// document.querySelector('.camera-button').addEventListener('click', function () {
+//     showPopupCamera();
+// });
